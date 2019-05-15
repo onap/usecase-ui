dde0f820699e639675a3564c8b4b20d2bfd750cc
guochuyicmri <guochuyi@chinamobile.com>
2019-05-15 19:31:57 +0800

    CCVPN Template parsing interface changes
    Change-Id: I3ea95928854c18943fbafb5c1dd515ec2e3ca57e
    Issue-ID: USECASEUI-220
    Signed-off-by: guochuyicmri <guochuyi@chinamobile.com>

--- usecaseui-portal/src/app/myhttp.service.ts
+++ usecaseui-portal/src/app/myhttp.service.ts
@@ -30,7 +30,7 @@ export class MyhttpService {
     serviceType: this.baseUrl + "/uui-lcm/customers/" + "*_*" + "/service-subscriptions",
     servicesTableData: this.baseUrl + '/uui-lcm/service-instances',
     serviceTemplates: this.baseUrl + "/uui-lcm/service-templates",
-    templateParameters: this.baseUrl + "/uui-lcm/fetchTemplateInfo",
+    templateParameters: this.baseUrl + "/uui-lcm/fetchCCVPNTemplateData/",
     e2etemplateParameters: this.baseUrl + "/uui-lcm/service-templates/" + "*_*" + "?toscaModelPath=",
     nstemplateParameters: this.baseUrl + "/uui-lcm/fetchNsTemplateData",
     vimInfo: this.baseUrl + "/uui-lcm/locations/",
@@ -109,7 +109,8 @@ export class MyhttpService {
             packageType: "Service",
             inputs: ""
         };
-        return this.http.post<any>(this.url.templateParameters, body);
+        let url = this.url.e2etemplateParameters.replace("*_*", template.uuid);
+        return this.http.post<any>(url, body);
     }
   }
 
