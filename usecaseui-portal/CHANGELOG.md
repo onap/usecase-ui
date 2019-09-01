19abe2af19ba49073949f8254aec897afe7bb303
cyuamber <xuranyjy@chinamobile.com>
2019-09-01 22:05:42 +0800

    docs: add copyright info and changelog
    Change-Id: Ie2c7aa97aaf99c7a274f5ca45366f549223ba028
    Issue-ID: USECASEUI-307
    Signed-off-by: cyuamber <xuranyjy@chinamobile.com>

--- usecaseui-portal/CHANGELOG.md
+++ usecaseui-portal/CHANGELOG.md
@@ -1,3 +1,29 @@
+# [1.0.0](https://gerrit.onap.org/r/usecase-ui/compare/2.0.1...1.0.0) (2019-09-01)
+
+
+### Bug Fixes
+
+* fix bugs of positions of README.md ([ebd842c](https://gerrit.onap.org/r/usecase-ui/commits/ebd842c))
+* fix bugs of table spinner and delete usless codes ([8455cd5](https://gerrit.onap.org/r/usecase-ui/commits/8455cd5))
+* fix the file path and delete console in mock data ([27b9342](https://gerrit.onap.org/r/usecase-ui/commits/27b9342))
+
+
+### Features
+
+* add ajax request function ([1c5c010](https://gerrit.onap.org/r/usecase-ui/commits/1c5c010))
+* add copyright and comments ([feb28bd](https://gerrit.onap.org/r/usecase-ui/commits/feb28bd))
+* add loading for page ([45fe372](https://gerrit.onap.org/r/usecase-ui/commits/45fe372))
+* add mock data routers config ([5d38fbf](https://gerrit.onap.org/r/usecase-ui/commits/5d38fbf))
+* change the mock data path config file ([0c1e82c](https://gerrit.onap.org/r/usecase-ui/commits/0c1e82c))
+* change the project structure and add mock data function ([d0f5347](https://gerrit.onap.org/r/usecase-ui/commits/d0f5347))
+* customer page code optimization ([41747c4](https://gerrit.onap.org/r/usecase-ui/commits/41747c4))
+* home page code optimization ([5d9b41e](https://gerrit.onap.org/r/usecase-ui/commits/5d9b41e))
+* Home page style optimization ([19a945d](https://gerrit.onap.org/r/usecase-ui/commits/19a945d))
+* optimization request function ([44b2c52](https://gerrit.onap.org/r/usecase-ui/commits/44b2c52))
+* optimize e2e instance creation page code ([8464ca4](https://gerrit.onap.org/r/usecase-ui/commits/8464ca4))
+
+
+
 # [1.0.0](https://gerrit.onap.org/r/usecase-ui/compare/2.0.1...1.0.0) (2019-08-23)
 
 
@@ -21,12 +47,4 @@
 * **mock:** this time, you can launch mock server easily by typing `npm run mock`!
 
 
-
-# [1.0.0](https://gerrit.onap.org/r/usecase-ui/compare/2.0.1...1.0.0) (2019-08-19)
-
-
-### Features
-
-* add loading for page ([45fe372](https://gerrit.onap.org/r/usecase-ui/commits/45fe372))
-* Home page style optimization ([19a945d](https://gerrit.onap.org/r/usecase-ui/commits/19a945d))
 
--- usecaseui-portal/src/app/shared/utils/http.ts
+++ usecaseui-portal/src/app/shared/utils/http.ts
@@ -1,32 +1,47 @@
+/*
+    Copyright (C) 2019 CMCC, Inc. and others. All rights reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 import axios from 'axios';
 
-
-export default function http (url:string,data:object = {},method:any = 'get',query?:string | object):any {
-  return new Promise((resolve,reject) => {
+export default function http(url: string, data: object = {}, method: any = 'get', query?: string | object): any {
+  return new Promise((resolve, reject) => {
     method = method.trim().toLocaleLowerCase()
-    let promise:any;
-    if(method === 'get' || method === 'delete'){
-      let options:object;
-      if(JSON.stringify(data) === '{}'){
+    let promise: any;
+    if (method === 'get' || method === 'delete') {
+      let options: object;
+      if (JSON.stringify(data) === '{}') {
         options = { method, url };
-      }else{
-        options = { method, url, params: data};
+      } else {
+        options = { method, url, params: data };
       }
 
-      if(method === 'delete'){
+      if (method === 'delete') {
 
       }
 
       promise = axios(options);
 
-    }else if (method === 'post' || method === 'put') {
-      if(method === 'post' && query){
-        let params:string = '';
-        if(<string>query){
+    } else if (method === 'post' || method === 'put') {
+      if (method === 'post' && query) {
+        let params: string = '';
+        if (<string>query) {
           query = JSON.parse((<string>query));
         }
         Object.keys(query).forEach(item => {
-          params += '&' + item + '=' + query[item]; 
+          params += '&' + item + '=' + query[item];
         })
         params = params.slice(1);
         url += '?' + params;
@@ -39,9 +54,9 @@ export default function http (url:string,data:object = {},method:any = 'get',que
     }
     promise
       .then((response) => {
-        if(response.status === 200 || 304){
+        if (response.status === 200 || 304) {
           resolve(response.data)
-        }else{
+        } else {
           reject(response)
         }
       })
