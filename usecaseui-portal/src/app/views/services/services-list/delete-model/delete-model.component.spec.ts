116cdd442196b1f249f0136310c666c76de076b9
Arundathi Patil <arundpil@in.ibm.com>
2019-11-07 15:10:49 +0530

    delete model component- added module and services
    Added module and service dependency to testbed configuration
    
    Issue-ID: USECASEUI-280
    Change-Id: I19989eea8285ad9e1e709ad694d074d7545cd4a5
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- usecaseui-portal/src/app/views/services/services-list/delete-model/delete-model.component.spec.ts
+++ usecaseui-portal/src/app/views/services/services-list/delete-model/delete-model.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 
 import { DeleteModelComponent } from './delete-model.component';
 
-describe('DeleteModelComponent', () => {
+fdescribe('DeleteModelComponent', () => {
   let component: DeleteModelComponent;
   let fixture: ComponentFixture<DeleteModelComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeleteModelComponent ]
+      declarations: [ DeleteModelComponent ],
+      imports: [ TranslateModule.forRoot(), NgZorroAntdModule.forRoot(), FormsModule ],
+      providers: [ { provide: NZ_I18N, useValue: en_US } ]
     })
     .compileComponents();
   }));
