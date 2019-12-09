ccd0ef2a8034cc1901d993f895951fde464dd471
cyuamber <xuranyjy@chinamobile.com>
2019-12-09 09:41:09 +0800

    feat: Added routing transition animation for slicing mgt
    Change-Id: I7aa51e27c755ccd1c2337dffdfe397a21f6fa64c
    Issue-ID: USECASEUI-368
    Signed-off-by: cyuamber <xuranyjy@chinamobile.com>

--- usecaseui-portal/src/app/views/services/slicing-management/slicing-management.component.ts
+++ usecaseui-portal/src/app/views/services/slicing-management/slicing-management.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { slideToRight } from '../../../shared/utils/animates';
 
 @Component({
   selector: 'app-slicing-management',
   templateUrl: './slicing-management.component.html',
-  styleUrls: ['./slicing-management.component.less']
+  styleUrls: ['./slicing-management.component.less'],
+  animations: [slideToRight]
 })
 export class SlicingManagementComponent implements OnInit {
+
+  @HostBinding('@routerAnimate') routerAnimateState;
 
   constructor() { }
 
