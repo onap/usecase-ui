e01eece2a41de041fc7092631b8dec12d226945b
cyuamber <xuranyjy@chinamobile.com>
2019-12-23 14:38:21 +0800

    feat:Optimize the compatibility of front-end data rendering
    Change-Id: I8336d68717c86f6b5268e70bfcd5fa0feda9d4a7
    Issue-ID: USECASEUI-369
    Signed-off-by: cyuamber <xuranyjy@chinamobile.com>

--- usecaseui-portal/src/app/views/services/slicing-management/slicing-resource-management/slicing-business-management/slicing-business-model/slicing-business-model.component.ts
+++ usecaseui-portal/src/app/views/services/slicing-management/slicing-resource-management/slicing-business-management/slicing-business-model/slicing-business-model.component.ts
@@ -37,9 +37,16 @@ export class SlicingBusinessModelComponent implements OnInit {
                     item = item.split(';').join('-');
                     return item
                 });
-                this.businessRequirement = [business_demand_info];
+                // area : Front-end analog data
+                let area = ["北京;北京市;海淀区", "北京;北京市;西城区", "北京;北京市;昌平区"].map(item => {
+                    item = item.split(';').join(' - ');
+                    return item
+                });
+                this.businessRequirement = [{...business_demand_info, area}];
                 this.NSTinfo = [nst_info];
-                this.nsiInfo = [nsi_info];
+                if(nsi_info.nsi_id !==null){
+                    this.nsiInfo = [nsi_info];
+                }
             }
         })
     }
