1c9f6c172596dbd52fdc14568c7b1865e8894840
cyuamber <xuranyjy@chinamobile.com>
2020-02-24 17:15:28 +0800

    feat:Public method extraction and optimization of CSMF slicing page
    Change-Id: Iac04b79b18d324b16c791e58ae40d3c93165a381
    Issue-ID: USECASEUI-368
    Signed-off-by: cyuamber <xuranyjy@chinamobile.com>

--- usecaseui-portal/src/app/shared/utils/utils.ts
+++ usecaseui-portal/src/app/shared/utils/utils.ts
@@ -31,4 +31,53 @@ export class Util {
         let formattime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
         return formattime;
     }
+    
+    getRulesText(words:string, title:string, index:number,rulesText:any[]){
+        return rulesText[index] = words + title
+    };
+    validator(item:object, val:any, index:number,rulesText:any[],validateRulesShow:any[]) {
+        let maxNumberReg = /^([1-9]\d{0,4}|100000)$/, // Check integer between 1 ~ 100000
+            expDataRateReg  = /^([1-9]\d{2}|[1-3]\d{3}|3000)$/, // Check integer between 100 ~ 3000
+            latencyReg = /^1[0-9]$|^[2-9]\d$|^1\d{2}$|^200$/; // Check integers between 10 and 200
+        if (val === null || val.replace(/\s*/g, '').length <= 0) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Please enter ', item.title, index,rulesText);
+            return false
+        } else {
+            validateRulesShow[index] = false;
+        }
+        if (isNaN(val) && item.key === 'maxNumberofUEs' && !maxNumberReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Only numbers can be entered', '', index,rulesText);
+            return false
+        } else if (!isNaN(val) && item.key === 'maxNumberofUEs' && !maxNumberReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Scope: 1-100000', '', index,rulesText);
+            return false
+        } else {
+            validateRulesShow[index] = false;
+        }
+        if (isNaN(val) && (item.key === 'expDataRateDL' || item.key === 'expDataRateUL') && !expDataRateReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Only numbers can be entered', '', index,rulesText);
+            return false
+        } else if ( !isNaN(val) && (item.key === 'expDataRateDL' || item.key === 'expDataRateUL') && !expDataRateReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Scope: 100-3000', '', index,rulesText);
+            return false
+        } else {
+            validateRulesShow[index] = false;
+        }
+        if (isNaN(val) && item.key === 'latency' && !latencyReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Only numbers can be entered', '', index,rulesText);
+            return false
+        } else if (!isNaN(val) && item.key === 'latency' && !latencyReg.test(val)) {
+            validateRulesShow[index] = true;
+            this.getRulesText('Scope: 10-200', '', index,rulesText);
+            return false
+        } else {
+            validateRulesShow[index] = false;
+        }
+    }
 }
