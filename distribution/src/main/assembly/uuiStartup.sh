86e490cc3dbf13fdd70d100d5461f4010cf5c15b
hekeguang <hekeguang@chinamobile.com>
2020-03-11 12:16:14 +0800

    Modify the startup related scripts to support the no root startup for the usecase-ui container!
    Issue-ID: USECASEUI-402
    Change-Id: I3931597155ae46a9aaff2e4be5760034606218d7
    Signed-off-by: hekeguang <hekeguang@chinamobile.com>

---
+++ distribution/src/main/assembly/uuiStartup.sh
@@ -0,0 +1,28 @@
+#
+# Copyright 2020 CMCC Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+echo "### Starting usecase-ui"
+
+cp -f /home/uui/server.xml /home/uui/tomcat/conf/
+echo "cp server.xml"
+
+cp -f /home/uui/web.xml /home/uui/tomcat/conf/
+echo "cp web.xml"
+
+cp -f /home/uui/uuiServer.jks /home/uui/tomcat/conf/
+echo "cp uuiServer.jks"
+
+bash /home/uui/tomcat/bin/catalina.sh run
