414d150327c692a115aa06414d8bf17509388943
wangyuerg <wangyuerg@chinamobile.com>
2021-03-05 20:37:03 +0800

    feat:modify style
    Signed-off-by: wangyuerg <wangyuerg@chinamobile.com>
    Change-Id: I3f6784469382e3fddeb6e27f756086d5f25f2d85
    Issue-ID: USECASEUI-527

--- usecaseui-portal/src/app/views/services/slicing-management/slicing-resource-management/slicing-business-management/slicing-business-model/slicing-business-model.component.ts
+++ usecaseui-portal/src/app/views/services/slicing-management/slicing-resource-management/slicing-business-management/slicing-business-model/slicing-business-model.component.ts
@@ -26,22 +26,6 @@ export class SlicingBusinessModelComponent implements OnInit {
     isSpinning: boolean = true;
     status: string = "";
     ngOnInit() {
-        console.log(this.businessId, "id",this.outerData);
-        const param = {
-            anVersion:'10.2.3.4.5',
-            vLanText:'111',
-            tnVersion:'10.0.1.1.1',
-            tnText:'TN EDGEText',
-            mbps:'333',
-            msText:'555',
-            linkType:'p2p/mp2p',
-            tnEndVersion:'10.1.1.1',
-            tnEndText:'TN EDGEText',
-            cnVlan:'66',
-            cnVersion:'1.1.2.3'
-        }
-        this.outerData = {...this.outerData,...param}
-        console.log('new',this.outerData)
         this.status = this.outerData.orchestration_status
         this.getDetail()
     }
@@ -68,7 +52,6 @@ export class SlicingBusinessModelComponent implements OnInit {
         })
     }
     showdetail(data) {
-        console.log(data, "data.nsi_id");
         const nsiModal = this.modalService.create({
             nzTitle: "Detail",
             nzContent: NsiModelComponent,
