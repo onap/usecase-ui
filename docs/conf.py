1cf6fe9e495ffc7044a84ff4ad1a0539f44a4412
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2022-11-02 09:26:41 +0530

    doc config files update
    Issue-ID: SO-4017
    
    Signed-off-by: Shashikanth.vh <shashikanth.vh@huawei.com>
    Change-Id: Iccffa68a8aac194ef23514ca8b1e9a6b7e3ef325
    (cherry picked from commit f384d73879f953e12bb8c4f90b9015299f3d0d66)

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,44 @@
 from docs_conf.conf import *
 
+project = "onap"
+release = "master"
+version = "master"
+
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "style_nav_header_background": "white",
+    "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+#
+# Map to 'latest' if this file is used in 'latest' (master) 'doc' branch.
+# Change to {releasename} after you have created the new 'doc' branch.
+#
+
 branch = 'latest'
+doc_url = 'https://docs.onap.org/projects'
+
 master_doc = 'index'
 
 linkcheck_ignore = [
-    'http://localhost',
+    r'http://localhost:\d+/'
+]
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.swaggerdoc',
+    'sphinxcontrib.plantuml'
 ]
 
 intersphinx_mapping = {}
@@ -13,3 +47,5 @@ html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
     app.add_css_file("css/ribbon.css")
+
+
