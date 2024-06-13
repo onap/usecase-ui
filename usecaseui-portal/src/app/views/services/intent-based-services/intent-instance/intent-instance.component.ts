83810e924f6eb18bb7a0771da25c7649026bd255
zhen1234 <liz779@chinatelecom.cn>
2024-06-13 11:45:49 +0800

    addexport
    Issue-ID: USECASEUI-837
    Signed-off-by: zhen1234 <liz779@chinatelecom.cn>
    Change-Id: Ib550bd47b32cddd08d99859aabfae2c8ec5f6264

--- usecaseui-portal/src/app/views/services/intent-based-services/intent-instance/intent-instance.component.ts
+++ usecaseui-portal/src/app/views/services/intent-based-services/intent-instance/intent-instance.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { NzMessageService } from "ng-zorro-antd";
 import { intentBaseService } from "../../../../core/services/intentBase.service";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-intent-instance',
@@ -11,41 +13,42 @@ import { intentBaseService } from "../../../../core/services/intentBase.service"
 export class IntentInstanceComponent implements OnInit {
 
   constructor(
-    private router:Router,
+    private router: Router,
+    private http: HttpClient,
     private myHttp: intentBaseService,
-    private nzMessage: NzMessageService 
-  ) {}
+    private nzMessage: NzMessageService
+  ) { }
 
-	ngOnChanges() {}
+  ngOnChanges() { }
 
-	ngOnInit() {
+  ngOnInit() {
     this.pageIndex = 1;
     this.pageSize = 10;
     this.getIntentionInstanceList();
   }
 
-	ngOnDestroy() {}
+  ngOnDestroy() { }
 
   // table lists
-	listOfData: any[] = [];
+  listOfData: any[] = [];
   // pageSize or pageNum
-	pageIndex: number = 1;
-	pageSize: number = 10;
-	total: number = 0;
-	loading = false;
-  
+  pageIndex: number = 1;
+  pageSize: number = 10;
+  total: number = 0;
+  loading = false;
+
   // init source data
-	getIntentionInstanceList(): void {
+  getIntentionInstanceList(): void {
     this.myHttp.getIntentInstanceList({
       currentPage: this.pageIndex,
       pageSize: this.pageSize
     }).subscribe((response) => {
-      const { code, message, data:{ totalRecords, list } } = response;
+      const { code, message, data: { totalRecords, list } } = response;
       if (code !== 200) {
         this.nzMessage.error(message);
-				return;
+        return;
       }
-      
+
       this.total = totalRecords;
       this.listOfData = list;
     }, (err) => {
@@ -65,7 +68,7 @@ export class IntentInstanceComponent implements OnInit {
       const { code, message, data } = response;
       if (code !== 200) {
         this.nzMessage.error(message);
-				return;
+        return;
       }
       this.nzMessage.success(data);
       this.resetParam2Query();
@@ -79,7 +82,7 @@ export class IntentInstanceComponent implements OnInit {
       const { code, message } = response;
       if (code !== 200) {
         this.nzMessage.error(message);
-				return;
+        return;
       }
       this.nzMessage.success('Delete IntentionInstance Success');
       this.resetParam2Query();
@@ -87,7 +90,26 @@ export class IntentInstanceComponent implements OnInit {
       console.log(err);
     });
   }
-
+  params: Object = {
+    intentId: '',
+    startDate: '',
+    endData: ''
+  };
+  exportData() {
+    this.http.post('/api/usecaseui-server/v1/intent/exportIntentContent', {}, { responseType: 'blob' })
+      .subscribe((data) => {
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(data);
+        link.setAttribute('download', 'excelData');
+        link.click();
+      }, (error) => {
+        console.error('export failed:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'export failed',
+        });
+      });
+  }
   resetParam2Query() {
     this.pageIndex = 1;
     this.pageSize = 10;
