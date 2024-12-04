91da062a4950f615b579182ea0331a827dfe45bd
kaixiliu <liukaixi@chinamobile.com>
2024-12-04 14:18:31 +0800

    1. Create a new descripition component code, and modify it to be the same as the Descriptions description list component style of the NG-ZORRO library 2. Optimize the code to extract a MaaS feature module and a common module. 3. Optimize the service code and create a new maasService file to store interface requests about maas
    Issue-ID: USECASEUI-844
    Change-Id: Ia72af1bf15947d77a1bf1a0a675f7a9bd1aa44b4
    Signed-off-by: kaixiliu <liukaixi@chinamobile.com>

--- usecaseui-portal/src/app/views/maas/knowledge-base-management/knowledge-base-detail/knowledge-base-detail.component.ts
+++ usecaseui-portal/src/app/views/maas/knowledge-base-management/knowledge-base-detail/knowledge-base-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { knowledgeBase } from '../knowledge-base.type';
+import { KnowledgeBase } from '../knowledge-base.type';
 import { KnowledgeBaseService } from '../knowledge-base.service';
 @Component({
   selector: 'app-knowledge-base-detail',
@@ -9,56 +9,22 @@ import { KnowledgeBaseService } from '../knowledge-base.service';
 export class KnowledgeBaseDetailComponent implements OnInit {
 
   constructor(
-    private knowledgeBaseService: KnowledgeBaseService
+    public knowledgeBaseService: KnowledgeBaseService
   ) { }
-  @Input() showModel: boolean;
-  _knowledgeBase: knowledgeBase;
-  data: Array<{ label: string, value: string }> = [];
-  @Input()
-
-  set knowledgeBaseDetail(v: any) {
-    if (!v) {
-      return;
-    }
-    this.data = [
-      {
-        label: 'Knowledge Base Name', value: v.knowledgeBaseName
-      },
-      {
-        label: 'Knowledge Base Description', value: v.knowledgeBaseDescription
-      },
-      {
-        label: 'Operator Name', value: v.operatorName
-      },
-      {
-        label: 'MaaS Platform Name', value: v.maaSPlatformName
-      },
-      {
-        label: 'Update Time', value: v.updateTime
-      },
-      {
-        label: 'Files Name', value: this.knowledgeBaseService.getFiles(v)
-      }
-    ]
-    this._knowledgeBase = v;
-  }
-
-  get knowledgeBaseDetail() {
-    return this._knowledgeBase;
-  };
+  @Input() showModal: boolean;
+  @Input() knowledgeBaseDetail: KnowledgeBase;
   @Output() modalOpreation = new EventEmitter();
-  files = '';
 
   ngOnInit() {
   }
 
   handleCancel(): void {
-    this.showModel = false;
+    this.showModal = false;
     this.modalOpreation.emit({ "cancel": true });
   }
 
   handleOk(): void {
-    this.showModel = false;
+    this.showModal = false;
     this.modalOpreation.emit({ "cancel": true });
   }
 }
